4772fe6c94d1f9dc11e0b369dd3eac1a5e880284
Dokoto Moloko <dokoto@MacBook-Air-de-Dokoto.local>
2013-10-05 18:54:52 +0200

    FUNCTIONALITY COMPLETE - ALPHA 0.1

--- src/net/bqtbl/lists/BookList.java
+++ src/net/bqtbl/lists/BookList.java
@@ -1,7 +1,7 @@
 package net.bqtbl.lists;
 
 import java.text.Collator;
-import java.text.Format;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -16,50 +16,33 @@ import com.dropbox.client2.session.Session.AccessType;
 import com.dropbox.client2.DropboxAPI.Entry;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
-import android.support.v4.view.GestureDetectorCompat;
 import android.view.GestureDetector;
-import android.view.GestureDetector.OnDoubleTapListener;
-import android.view.GestureDetector.OnGestureListener;
-import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 import android.app.ListActivity;
 import android.content.Context;
-import net.bqtbl.lists.MyGestureDetector;
+import net.bqtbl.utl.ActionBookManager;
+import net.bqtbl.structs.Adapter_row;
 
-
+ 
 public class BookList extends ListActivity {
 
-	final static private String APP_KEY = "b7bmniiymc8xzs5";
-	final static private String APP_SECRET = "9ky56d1uqoxsg4m";
+	final static private String APP_KEY = "5fwf3fhkg93g2ix";
+	final static private String APP_SECRET = "71xo6y4v1617wwx";
 	final static private AccessType ACCESS_TYPE = AccessType.DROPBOX;
 	List<Entry> FileList;
 	private DropboxAPI<AndroidAuthSession> mDBApi;
 	private ListQuestionsAdapter adapter;
-
-	
-	private class Adapter_row
-	{
-
-		public Adapter_row(String filename, String filedate)
-		{
-			this.filename = filename;
-			this.filedate = filedate;
-		}		
-		public String filename;
-		public String filedate;
-	}
+	private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss", Locale.getDefault());
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -74,13 +57,23 @@ public class BookList extends ListActivity {
 	}
 	
 	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event)
+	{
+		if ((keyCode == KeyEvent.KEYCODE_BACK))
+		{	        
+			mDBApi.getSession().unlink();
+			this.finish();			
+		}
+		return super.onKeyDown(keyCode, event);
+	}	
+	
+	@Override
 	protected void onResume() {
 	    super.onResume();
 
 	    if (mDBApi.getSession().authenticationSuccessful()) {
 	        try {
 	            mDBApi.getSession().finishAuthentication();
-	            //AccessTokenPair tokens = 
 	            mDBApi.getSession().getAccessTokenPair();
 	            GetFile(mDBApi);
 	        } catch (Exception e) {
@@ -164,7 +157,13 @@ public class BookList extends ListActivity {
 		case R.id.sortByDate:
 			adapter.sort(new Comparator<Adapter_row>() {
 			    public int compare(Adapter_row arg0, Adapter_row arg1) {
-			       return col.compare(arg0.filedate, arg1.filedate);
+			    	try {
+						return dateFormat.parse(arg0.filedate).compareTo(dateFormat.parse(arg1.filedate));
+					} catch (ParseException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+						return -1;
+					}
 			    }
 			});
 			adapter.setNotifyOnChange(true);
@@ -184,15 +183,18 @@ public class BookList extends ListActivity {
 	public class ListQuestionsAdapter extends ArrayAdapter<Adapter_row>
 	{
 		private final Context context;
-		ViewHolder holder;
-		ArrayList<Adapter_row> rows;
-		Format formatter = new SimpleDateFormat("dd/MM/yyyy HH:MM:SS", Locale.getDefault());
+		private ViewHolder holder;
+		public ArrayList<Adapter_row> rows;
+		private GestureDetector mDetector;
+		private ActionBookManager am;
 
 		public ListQuestionsAdapter(Context context, ArrayList<Adapter_row> Rows)
 		{
 			super(context, R.layout.layo_row_booklist, Rows);
 			this.context = context;
 			this.rows = Rows;
+			am = new ActionBookManager(BookList.this, mDBApi, rows);			
+			mDetector = new GestureDetector(am);
 		}
 				
 		@Override
@@ -214,26 +216,18 @@ public class BookList extends ListActivity {
 			holder = (ViewHolder) rowView.getTag();			
 			holder.tv_filename.setText(rows.get(position).filename);
 			holder.tv_filedate.setText(rows.get(position).filedate);
-			holder.iv_fileico.setTag(Integer.valueOf(position));
-			holder.iv_fileico.setOnClickListener(new OnClickListener()
-			{
-				@Override
-				public void onClick(final View view)
-				{
-					int position = (Integer) view.getTag();
-					//Toast.makeText(BookList.this, "Click on Ico : " + String.valueOf(position), Toast.LENGTH_SHORT).show();
-					notifyDataSetChanged();
-				}
-			}); 
 			
+			
+			holder.iv_fileico.setTag(Integer.valueOf(position));
 			holder.iv_fileico.setOnTouchListener(new OnTouchListener() {
-	             GestureDetector gestureDetector = new GestureDetector(new MyGestureDetector(BookList.this));
-	             @Override
-	             public boolean onTouch(View v, MotionEvent event) {
-	                    return gestureDetector.onTouchEvent(event);
-	             }
-	    });
-
+				@Override
+				public boolean onTouch(View v, MotionEvent event)
+				{
+					int position = (Integer) v.getTag();
+					am.SetPosition(position);
+				    mDetector.onTouchEvent(event);				    
+				    return true;
+				}});
 			
 			return rowView;
 		}
